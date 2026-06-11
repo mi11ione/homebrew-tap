912c663fb2fea441e42fa2b2cf59a37a682eadaa
mi11ione <i@mi11ion.io>
2026-06-11 19:14:24 +0200

    iris 0.1.0: fill release checksums

--- Formula/iris.rb
+++ Formula/iris.rb
@@ -9,17 +9,17 @@ class Iris < Formula
 
   on_macos do
     url "https://github.com/mi11ione/iris/releases/download/0.1.0/iris-0.1.0-macos-universal.tar.gz"
-    sha256 "FILL_AFTER_FIRST_RELEASE_MACOS"
+    sha256 "63ea5c1ee3ad5bf7eedbaa8a7a33870a41872f64ed3f4ec8f47982ed02e2b02c"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/mi11ione/iris/releases/download/0.1.0/iris-0.1.0-linux-x86_64.tar.gz"
-      sha256 "FILL_AFTER_FIRST_RELEASE_LINUX_X86_64"
+      sha256 "b451b702fec1978745fdb65f674932a77f01bc61b94156287708cadeff438de8"
     end
     on_arm do
       url "https://github.com/mi11ione/iris/releases/download/0.1.0/iris-0.1.0-linux-aarch64.tar.gz"
-      sha256 "FILL_AFTER_FIRST_RELEASE_LINUX_AARCH64"
+      sha256 "a58e2dbfc0d96476e66c285d8a86b08744cecfa9774b1ece8edc1212f59e9090"
     end
   end
 
