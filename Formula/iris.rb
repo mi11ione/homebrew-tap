274a73e021888d5b22cee16029f579e1170eb849
mi11ione <i@mi11ion.io>
2026-06-11 18:08:22 +0200

    iris 0.1.0 formula (checksums filled at first release)

---
+++ Formula/iris.rb
@@ -0,0 +1,34 @@
+# Copyright (c) 2026 Roman Zhuzhgov
+# Licensed under the Apache License, Version 2.0
+
+class Iris < Formula
+  desc "Pure-Swift ARM64/ARM64E disassembler with a corpus-proven semantic layer"
+  homepage "https://github.com/mi11ione/iris"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  on_macos do
+    url "https://github.com/mi11ione/iris/releases/download/0.1.0/iris-0.1.0-macos-universal.tar.gz"
+    sha256 "FILL_AFTER_FIRST_RELEASE_MACOS"
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/mi11ione/iris/releases/download/0.1.0/iris-0.1.0-linux-x86_64.tar.gz"
+      sha256 "FILL_AFTER_FIRST_RELEASE_LINUX_X86_64"
+    end
+    on_arm do
+      url "https://github.com/mi11ione/iris/releases/download/0.1.0/iris-0.1.0-linux-aarch64.tar.gz"
+      sha256 "FILL_AFTER_FIRST_RELEASE_LINUX_AARCH64"
+    end
+  end
+
+  def install
+    bin.install "iris"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/iris --version")
+    assert_match "nop", shell_output("#{bin}/iris 0xd503201f")
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# homebrew-tap
+
+Homebrew formulae for [mi11ione](https://github.com/mi11ione)'s tools.
+
+```sh
+brew install mi11ione/tap/iris
+```
+
+| formula | what it is |
+|---|---|
+| `iris` | Pure-Swift ARM64/ARM64E disassembler with a corpus-proven semantic layer ([repo](https://github.com/mi11ione/iris)) |
+
+Formulae pour prebuilt binaries from each project's GitHub Releases and
+verify their checksums.
